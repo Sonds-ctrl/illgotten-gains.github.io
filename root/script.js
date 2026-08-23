a72bcd4cf50e2c7a35c703c737957ed5821d2a96
Sonds-ctrl <savannahasimmonds@gmail.com>
2026-08-22 21:43:55 -0400

    Delete root directory

--- root/script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() { ScrollIndicator() };
-
-function ScrollIndicator() {
-	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-	var scrolled = (winScroll / height) * 100;
-	document.getElementById( 'footer__progress_bar' ).style.width = scrolled + "%";
-	document.getElementById( 'footer__progress_bar' ).innerHTML = Math.round( scrolled ) + "%"
-}
